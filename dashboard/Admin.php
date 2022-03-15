cec11f0802a4c1d0727709c73d5ce3e982f38ac6
NielTatad <king30cinco@gmail.com>
2022-03-15 15:53:50 +0800

    adding fa-fa-bell

--- dashboard/Admin.php
+++ dashboard/Admin.php
@@ -195,9 +195,9 @@ th {
                     </div>
                   </li>
   
-                  <li role="presentation" class="nav-item dropdown open">
-                    <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
-                      <i class="fa fa-envelope-o"></i>
+                  <li role="presentation" class="nav-item dropdown open" style="margin-top:6px;">
+                    <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false" >
+                      <i class="fa fa-bell"></i>
                       <!--<span class="badge bg-green">6</span>-->
                     </a>
                     <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
--- dashboard/AnimalWelfareOrganizationDashboard.php
+++ dashboard/AnimalWelfareOrganizationDashboard.php
@@ -302,9 +302,9 @@ if($query->rowCount()>0)
                     </div>
                   </li>
   
-                  <li role="presentation" class="nav-item dropdown open">
-                    <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
-                      <i class="fa fa-envelope-o"></i>
+                  <li role="presentation" class="nav-item dropdown open" style="margin-top:6px;">
+                    <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false" >
+                      <i class="fa fa-bell"></i>
                        <!--<span class="badge bg-green">6</span>-->
                     </a>
                     <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
