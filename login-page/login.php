61a1f09467d0008c528b50c4009c32a1f9e0c968
NielTatad <king30cinco@gmail.com>
2022-03-28 22:02:30 +0800

    adding prevention for back login

--- login-page/login.php
+++ login-page/login.php
@@ -135,6 +135,9 @@ if(isset($_POST['login']))
 <!doctype html>
 <html lang="en">
   <head>
+  <script language="javascript" type="text/javascript">
+    window.history.forward();
+    </script>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
