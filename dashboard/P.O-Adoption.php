f796087192c2e40b7a31dcf60b3e85bb67bd5ad8
NielTatad <92833937+NielTatad@users.noreply.github.com>
2022-04-11 00:33:57 +0800

    push new changes

--- dashboard/P.O-Adoption.php
+++ dashboard/P.O-Adoption.php
@@ -1206,27 +1206,6 @@ if(isset($_POST['btnComment']))
   }).hide();
   </script>
 
-	<!-- Javascript functions	-->
-	<script>
-		function hideshow(){
-			var password = document.getElementById("password1");
-			var slash = document.getElementById("slash");
-			var eye = document.getElementById("eye");
-			
-			if(password.type === 'password'){
-				password.type = "text";
-				slash.style.display = "block";
-				eye.style.display = "none";
-			}
-			else{
-				password.type = "password";
-				slash.style.display = "none";
-				eye.style.display = "block";
-			}
-
-		}
-	</script>
-
     <script>
         // initialize a validator instance from the "FormValidator" constructor.
         // A "<form>" element is optionally passed as an argument, but is not a must
